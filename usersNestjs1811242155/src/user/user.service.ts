76075fb24e79fe4bfcefce61503b6b4a73e66a2a
Leon3854 <38610733+Leon3854@users.noreply.github.com>
2025-06-24 21:44:47 +0300

    Update user.service.ts

--- usersNestjs1811242155/src/user/user.service.ts
+++ usersNestjs1811242155/src/user/user.service.ts
@@ -5,12 +5,16 @@ import { PrismaService } from '../../prisma/prisma.service';
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
+  // Сбрасываем флаг hasProblems для всех пользователей и возвращаем количество обновленных записей
   async resetProblemsFlag(): Promise<number> {
+    /// Находим всех пользователей с установленным флагом hasProblems = true
     const usersWithProblems = await this.prisma.user.findMany({
       where: { hasProblems: true },
     });
+    // Получаем количество таких пользователей
     const count = usersWithProblems.length;
 
+   // Обновляем всех пользователей, у которых hasProblems = true, сбрасывая этот флаг (устанавливаем false)
     await this.prisma.user.updateMany({
       where: { hasProblems: true },
       data: { hasProblems: false },
